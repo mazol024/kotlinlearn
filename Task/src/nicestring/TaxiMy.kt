07f85983b39f1a5b0de775c11544bfecad540565
mazol024 <38819166+mazol024@users.noreply.github.com>
2021-07-30 13:36:41 +1200

    Update TaxiMy.kt

--- Task/src/nicestring/TaxiMy.kt
+++ Task/src/nicestring/TaxiMy.kt
@@ -1,26 +1,23 @@
 package taxipark
 
 fun main(){
-    var tp = taxiPark(0..6, 0..9,
+    var tp1 =  taxiPark(0..6, 0..9,
         trip(2, listOf(9), duration = 9, distance = 36.0),
-        trip(1, listOf(0,2,5), duration = 15, distance = 28.0),
+        trip(1, listOf(0,7), duration = 15, distance = 28.0),
         trip(2, listOf(1), duration = 37, distance = 30.0),
-        trip(0, listOf(5,9), duration = 24, distance = 10.0),
-        trip(5, listOf(1,4,7,9), duration = 24, distance = 10.0),
+        trip(0, listOf(9), duration = 24, distance = 10.0),
         trip(1, listOf(2), duration = 1, distance = 6.0),
         trip(0, listOf(0, 9), duration = 9, distance = 7.0),
-        trip(2, listOf(3, 2, 8), duration = 18, distance = 39.0, discount = 0.1),
-        trip(1, listOf(9, 4), duration = 19, distance = 1.0, discount = 0.2),
-        trip(1, listOf(2,3), duration = 16, distance = 23.0),
-        trip(2, listOf(5,7), duration = 10, distance = 31.0, discount = 0.2))
+        trip(2, listOf(1,2,3,7,8), duration = 18, distance = 39.0, discount = 0.1),
+        trip(1, listOf(1,3,9,4), duration = 19, distance = 1.0, discount = 0.2),
+        trip(1, listOf(3), duration = 16, distance = 23.0),
+        trip(5, listOf(3,5,2), duration = 16, distance = 123.0),
+        trip(2, listOf(3,7), duration = 10, distance = 31.0, discount = 0.2))
+    println(" test 1 ${tp1.findFake()}")
+    println(" test 2 ${tp1.findFaith(3)}")
+    println(" test 3 ${tp1.findSmartPass()}")
 
-
-    println("Drivers fake test  ${tp.findFake()}")
-    println("Passenger test  ${tp.findFaith(2)}")
-    println("Frequent Passenger test  ${tp.findFrequentPass(Driver("D-1"))}")
 }
-
-//fun
 fun TaxiPark.findFake(): Set<Driver> =
     this.allDrivers.minus(
         this.trips.map { it.driver }).toSet()
@@ -36,4 +33,18 @@ fun TaxiPark.findFrequentPass(driver: Driver):List<Passenger> =
         .groupingBy { it.name }.eachCount()
         .filter { it.value > 2 }
         .keys.map { Passenger(it) }
+
+fun TaxiPark.findSmartPass(): Collection<Passenger> =
+    this.trips.filter { it.discount != null  }
+        .flatMap { it.passengers }
+        .groupingBy { it.name }
+        .eachCount()
+        .filter { it1 ->
+            this
+                .trips
+                .filter { it.discount == null  }
+                .flatMap { it.passengers }
+                .groupingBy { it.name }
+                .eachCount().getOrDefault(it1.key,0) < it1.value
+        }.keys.map { Passenger(it) }
 
